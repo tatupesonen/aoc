c4a560ab3e9c5752eaf59d1d55f6587fc0276cdd
Tatu Pesonen <tatu@narigon.dev>
2022-12-03 18:09:45 +0200

    fix: format with spaces instead of tab

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(iter_array_chunks)]
 
-use clap::{Parser, arg};
+use clap::{arg, Parser};
 mod days;
 
 pub trait Solution {
@@ -9,37 +9,38 @@ pub trait Solution {
 }
 
 fn select_day(day: usize) -> (usize, Box<dyn Solution>) {
-	let solution: Box<dyn Solution> = match day {
-		1 => Box::new(days::day1::Problem),
-		2 => Box::new(days::day2::Problem),
-		3 => Box::new(days::day3::Problem),
-		_ => panic!("no such day")
-	};
+    let solution: Box<dyn Solution> = match day {
+        1 => Box::new(days::day1::Problem),
+        2 => Box::new(days::day2::Problem),
+        3 => Box::new(days::day3::Problem),
+        _ => panic!("no such day"),
+    };
 
-	(day, solution)
+    (day, solution)
 }
 
 fn run_day(input: &str, day: Box<dyn Solution>) -> () {
-	let part1 = day.part_one(input);
-	println!("Part 1: {}", part1);
-	let part2 = day.part_two(input);
-	println!("Part 2: {}", part2);
+    let part1 = day.part_one(input);
+    println!("Part 1: {}", part1);
+    let part2 = day.part_two(input);
+    println!("Part 2: {}", part2);
 }
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// Day to run
-   #[arg(short, long)]
-   day: usize,
+    /// Day to run
+    #[arg(short, long)]
+    day: usize,
 }
 
 fn main() {
-	let args = Args::parse();
-	let (day, solution) = select_day(args.day);
-	// Get input
-	let input = std::fs::read_to_string(format!("./inputs/{}/input.txt", day)).expect("Input file doesn't exist.");
-	println!("****** Solutions for day {} ******", day);
-	run_day(&input, solution);
+    let args = Args::parse();
+    let (day, solution) = select_day(args.day);
+    // Get input
+    let input = std::fs::read_to_string(format!("./inputs/{}/input.txt", day))
+        .expect("Input file doesn't exist.");
+    println!("****** Solutions for day {} ******", day);
+    run_day(&input, solution);
 }
