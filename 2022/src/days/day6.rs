597f78c04564ea01afcef36e27386dc9eadeb1ba
Tatu Pesonen <tatu@narigon.dev>
2022-12-06 15:13:02 +0200

    refactor: cleanup day6 by moving find_marker to a subroutine

--- 2022/src/days/day6.rs
+++ 2022/src/days/day6.rs
@@ -4,39 +4,31 @@ use std::collections::HashSet;
 const PART1_SLICE_SIZE: usize = 4;
 const PART2_SLICE_SIZE: usize = 14;
 
+fn find_marker(it: Vec<char>, slice_size: usize) -> usize {
+    let it = it.windows(slice_size).enumerate();
+    let mut found = 0;
+    for (idx, slice) in it {
+        let set = slice.iter().collect::<HashSet<&char>>();
+        if set.len() == slice_size {
+            found = idx + slice_size;
+            break;
+        }
+    }
+		found
+}
+
 pub struct Problem;
 impl Solution for Problem {
     fn part_one(&self, input: &str) -> String {
         // Collect to vec so we get `.windows()`. For some reason `Chars<_>` does not implement it.
         let output: Vec<char> = input.chars().into_iter().collect();
-        let it = output.windows(PART1_SLICE_SIZE).enumerate();
-
-        let mut found = 0;
-        for (idx, slice) in it {
-            let set = slice.iter().collect::<HashSet<&char>>();
-            if set.len() == PART1_SLICE_SIZE {
-                found = idx + PART1_SLICE_SIZE;
-                break;
-            }
-        }
-
+				let found = find_marker(output, PART1_SLICE_SIZE);
         found.to_string()
     }
 
     fn part_two(&self, input: &str) -> String {
-        // Collect to vec so we get `.windows()`. For some reason `Chars<_>` does not implement it.
         let output: Vec<char> = input.chars().into_iter().collect();
-        let it = output.windows(PART2_SLICE_SIZE).enumerate();
-
-        let mut found = 0;
-        for (idx, slice) in it {
-            let set = slice.iter().collect::<HashSet<&char>>();
-            if set.len() == PART2_SLICE_SIZE {
-                found = idx + PART2_SLICE_SIZE;
-                break;
-            }
-        }
-
+				let found = find_marker(output, PART2_SLICE_SIZE);
         found.to_string()
     }
 }
