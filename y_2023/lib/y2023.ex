956be331f2de5383b3daa84af3edbcc8c5c5e327
Tatu Pesonen <tatu@narigon.dev>
2023-11-23 19:25:39 +0200

    feat: init for 2023

---
+++ y_2023/lib/y2023.ex
@@ -0,0 +1,18 @@
+defmodule Y2023 do
+  @moduledoc """
+  Documentation for `Y2023`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Y2023.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ y_2023/test/y2023_test.exs
@@ -0,0 +1,8 @@
+defmodule Y2023Test do
+  use ExUnit.Case
+  doctest Y2023
+
+  test "greets the world" do
+    assert Y2023.hello() == :world
+  end
+end
